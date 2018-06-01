42bdcedd2b9bb8f4ef8a67978d4d9887d9efaa7d
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-02 00:05:21 +0100

    Created a new template

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,15 +6,49 @@ var template = React.createElement(
     React.createElement(
         'h1',
         null,
-        'Indecision App'
+        'Enoy Moreno Jimenez'
     ),
     React.createElement(
         'p',
         null,
         'This is some text'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Yone Moreno Jimenez'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: 21'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Las Palmas'
     )
 );
 
 var app = document.getElementById('app');
 
-ReactDOM.render(template, app);
+ReactDOM.render(templateTwo, app);
--- src/app.js
+++ src/app.js
@@ -1,10 +1,22 @@
 var template = (
     <div>
-        <h1>Indecision App</h1>
+        <h1>Enoy Moreno Jimenez</h1>
         <p>This is some text</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
+    </div>
+);
+
+const templateTwo = (
+    <div>
+        <h1>Yone Moreno Jimenez</h1>
+        <p>Age: 21</p>
+        <p>Location: Las Palmas</p>
     </div>
 );
 
 const app = document.getElementById('app');
 
-ReactDOM.render(template, app);
+ReactDOM.render(templateTwo, app);
