b24b06c4e2e8544e4923d400c7787d44362f52cc
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-02 14:06:41 +0100

    Clases es6

---
+++ src/playground/es6-classes1.js
@@ -0,0 +1,21 @@
+class Person {
+    constructor(name = 'anonymous', age = 0) {
+        this.name = name;
+        this.age = age;
+    }
+
+    getGreeting() {
+        // return 'Hola ' + this.name + '!';
+        return `Hello!!! I am ${this.name}`;
+    }
+
+    getDescription() {
+        return `${this.name} is ${this.age} year(s) old`;
+    }
+}
+
+const me = new Person('Tori Guillen', 50);
+const me2 = new Person();
+console.log(me, me2);
+console.log(me.getGreeting());
+console.log(me.getDescription());
