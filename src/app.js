f27043689c084401ac73823f4891fad8a8e3211f
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-02 15:36:14 +0100

    Add top level component IndecisionApp and add a Option component nested into Options ;=)

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+class IndecisionApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+                <Action/>
+                <Options/>
+                <AddOption/>
+            </div>
+        );
+    }
+}
+
 class Header extends React.Component {
     render() {
         return (
@@ -24,9 +37,7 @@ class Options extends React.Component {
         return (
             <div>
                 <ol>
-                    <li>Option One</li>
-                    <li>Option Two</li>
-                    <li>Option 3</li>
+                    <Option/>
                 </ol>
             </div>
         );
@@ -46,12 +57,14 @@ class AddOption extends React.Component {
     }
 }
 
-const jsx = (
-    <div>
-        <Header/>
-        <Action/>
-        <Options/>
-        <AddOption/>
-    </div>
-);
-ReactDOM.render(jsx, document.getElementById('app'));
+class Option extends React.Component {
+    render() {
+        return (
+            <div>
+                <li>Option here</li>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
