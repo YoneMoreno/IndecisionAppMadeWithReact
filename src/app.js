9e01eae72163a7d8063b9f7778385c56b50cabd9
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-02 15:23:29 +0100

    Add two new cclasses

--- src/app.js
+++ src/app.js
@@ -19,10 +19,39 @@ class Action extends React.Component {
     }
 }
 
+class Options extends React.Component {
+    render() {
+        return (
+            <div>
+                <ol>
+                    <li>Option One</li>
+                    <li>Option Two</li>
+                    <li>Option 3</li>
+                </ol>
+            </div>
+        );
+    }
+}
+
+class AddOption extends React.Component {
+    render() {
+        return (
+            <div>
+                <form action="">
+                    <input type="text" name="option"/>
+                    <button type="submit">Add a new option</button>
+                </form>
+            </div>
+        );
+    }
+}
+
 const jsx = (
     <div>
         <Header/>
         <Action/>
+        <Options/>
+        <AddOption/>
     </div>
 );
 ReactDOM.render(jsx, document.getElementById('app'));
