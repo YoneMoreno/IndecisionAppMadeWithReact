0c5a6c3322c524b83982a6de4a09aaf31cbdd2eb
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 09:34:33 +0100

    If we had to store the function using the old syntax and then execute it it would fail because of this binding has been broken

--- src/app.js
+++ src/app.js
@@ -4,3 +4,23 @@ import IndecisionApp from "./components/indecisionApp";
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
 
+class OldSyntax {
+    constructor() {
+        this.name = 'Rufo';
+    }
+
+    getGreeting() {
+        return `Hi! My name is ${this.name}`;
+    }
+}
+
+const oldSyntax = new OldSyntax();
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+class NewSyntax {
+    name = 'Ainara';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
