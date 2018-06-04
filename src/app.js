dd70d60efbb095f434624cdbc626e96c57c7a811
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 09:56:00 +0100

    Our first step to know how to ps in dinamicaally elements to our react pages

--- src/app.js
+++ src/app.js
@@ -2,31 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/indecisionApp";
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+const Layout = () => {
+    return (
+        <div>
+            <p>Header</p>
+            <p>Footer</p>
+        </div>
+    );
+};
 
-class OldSyntax {
-    constructor() {
-        this.name = 'Rufo';
-        this.getGreeting = this.getGreeting.bind(this);
-    }
 
-    getGreeting() {
-        return `Hi! My name is ${this.name}`;
-    }
-}
-
-const oldSyntax = new OldSyntax();
-const getGreeting = oldSyntax.getGreeting;
-console.log(getGreeting());
-
-class NewSyntax {
-    name = 'Ainara';
-
-    getGreeting = () => {
-        return `Hi hi!. My name is: ${this.name}`
-    }
-}
-
-const newSyntax = new NewSyntax();
-const getGreetingNew = newSyntax.getGreeting;
-console.log(getGreetingNew());
+ReactDOM.render(<Layout/>, document.getElementById('app'));
