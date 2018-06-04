4a4a95a7f227e76de1954e79c8b8b2e0f3f670e8
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 07:25:33 +0100

    We can omit to declare a new const

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,6 @@ export const square = (x) => x * x;
 
 export const add = (x, y) => x + y;
 
-const substract = (a, b) => a - b;
-export default substract;
+export default (a, b) => a - b;
 
 // export {square, add, substract as default};
