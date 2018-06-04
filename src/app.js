016aeb2206782d60b33f1e242740892ffc48c3f6
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 10:05:05 +0100

    return back to IndecisionApp

--- src/app.js
+++ src/app.js
@@ -2,20 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/indecisionApp";
 
-const Layout = (props) => {
-    return (
-        <div>
-            <p>Header</p>
-            {props.children}
-            <p>Footer</p>
-        </div>
-    );
-};
 
-
-ReactDOM.render(
-    (<Layout>
-        <h1>Title</h1>
-        <p>Page content!!</p>
-    </Layout>),
-    document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
