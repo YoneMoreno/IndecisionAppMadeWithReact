b3e1c9516286acce62662a5fdee58986d0fe4ac3
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 08:46:10 +0100

    It is better to bootstrap the app directly ;=)

--- src/app.js
+++ src/app.js
@@ -2,11 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/indecisionApp";
 
-const App = () => {
-    return (
-        <IndecisionApp/>
-    );
-};
-
-
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
