d7b9b62cd0f5e3c755722503978597a836560f8e
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 09:58:17 +0100

    We can inject dinamically content to the layour of our react age

--- src/app.js
+++ src/app.js
@@ -2,14 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/indecisionApp";
 
-const Layout = () => {
+const Layout = (props) => {
     return (
         <div>
             <p>Header</p>
+            {props.content}
             <p>Footer</p>
         </div>
     );
 };
 
+const template =
+    <div>
+        <h1>Title</h1>
+        <p>Page content!!</p>
+    </div>;
 
-ReactDOM.render(<Layout/>, document.getElementById('app'));
+
+ReactDOM.render(<Layout content={template}/>, document.getElementById('app'));
