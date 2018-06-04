593903a953f34da18b718fa245a4b87edf638c11
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 13:31:21 +0100

    We have put the title and remove all buttons inside the widget header of the Options list one in the left and the other in the right. The trick is to use display flex and justify ontent space between ;=) ;=) ;=) ;=)

--- src/components/Options.js
+++ src/components/Options.js
@@ -6,7 +6,7 @@ const Options = (props) =>
     (
         <div>
             <div className="widget-header">
-                <h3>Here are your options ;=)</h3>
+                <h3 className="widget-header__title">Here are your options ;=)</h3>
                 <button
                     className="button button--link"
                     onClick={props.handleDeleteOptions}
