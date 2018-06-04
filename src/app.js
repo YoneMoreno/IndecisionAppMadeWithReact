c235a11d183be95b4b9d11c4ab7720e987cbb18c
Yone <yone.moreno101@alu.ulpgc.es>
2018-06-04 09:27:52 +0100

    We have added the transform-class-properties babel plugin to use the lastest js feature to write shorther class properties

--- src/app.js
+++ src/app.js
@@ -3,3 +3,19 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from "./components/indecisionApp";
 
 ReactDOM.render(<IndecisionApp/>, document.getElementById('app'));
+
+class OldSyntax {
+    constructor() {
+        this.name = 'Rufo';
+    }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+
+class NewSyntax {
+    name = 'Ainara';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
